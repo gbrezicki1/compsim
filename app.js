dfb4b83f747a95297818c72c8b6e99ee6f172cd0
Gordon Brezicki <gbrezicki@gmail.com>
2024-09-30 07:18:12 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
