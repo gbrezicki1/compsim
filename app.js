776785bc2bb5575277b2a331dd59f5d34d1c01ca
Gordon Brezicki <gbrezicki@gmail.com>
2024-10-01 06:32:50 -0400

    initial setup of server, folders, etc.

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+//import bodyParser from "body-parser";
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+import { And } from "./lib/and";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+    res.render("index.ejs", {
+        input1Initial: false,
+        input2Initial: false,
+        output: false
+    });
 });
+
+app.post("/submit", (req,res) => {
+    let and = new And();
+    and.update(input1=req.body.input1, input2=req.body.input2);
+    res.render("index.ejs",{
+        input1Initial: req.body.input1,
+        input2Initial: req.body.input2,
+        output: and.output
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
