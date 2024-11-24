67d09397a38c7b4832e98bf705b90762f37575ba
Gordon Susteon <gpb@susteoninc.com>
2024-11-24 09:40:22 -0500

    debug Ram16 class #2

--- testscripts/test_ram16.js
+++ testscripts/test_ram16.js
@@ -1,4 +1,11 @@
 import { Ram16 } from "../lib/ram16.js";
 
 let ram = new Ram16(8);
+ram.addr = [1,0,0,0,0,0,0,0];
+ram.write = 1;
+ram.read = 1;
+ram.addrSet = 1;
+ram.input = [1,1,1,1,1,1,1,1];
+ram.update();
+console.log(ram.output);
 console.log();
